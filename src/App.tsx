836eebcc3cc8e468dd7170da6700f5848711d98f
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-03-14 00:32:55 +0100

    master

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,59 @@ import { Learn } from './pages/Learn';
 import Learned from './pages/Learned';
 import Test from './pages/Test';
 
+import { setDoc, doc, collection, getDocs } from "firebase/firestore";
+import { signInWithPopup, onAuthStateChanged } from "firebase/auth";
+import { auth, provider, db } from "./config/firebase";
+import { useEffect } from "react";
+
 function App() {
 
+  interface Kanji {
+    character?: string;
+    meanings?: string[];
+    freq?: number;
+    grade?: number;
+    jlpt_new?: number;
+    jlpt_old?: number;
+    category?: string;
+    strokes?: number;
+    
+  }
+  
+  const saveDataBeforeUnload = () => {
+    const storedKanji = JSON.parse(sessionStorage.getItem("learnedKanjiArray") || "[]") as Kanji[];
+    storedKanji.forEach((kanji: Kanji) => {
+      handleSaveKanji(kanji);
+    });   
+  };
+  
+  const handleSaveKanji = async (kanji: Kanji) => {
+    const currentUser = auth.currentUser?.uid;
+    if (currentUser) {
+      const learnedRef = collection(db, "users", currentUser, "learned");
+      const docRef = doc(learnedRef, kanji.character);
+      await setDoc(docRef, { kanji });
+      console.log("aaaaa");
+    }  
+
+  };
+  
+  useEffect(() => {
+    const handleBeforeUnload = (event: BeforeUnloadEvent) => {
+      saveDataBeforeUnload();
+      event.preventDefault();
+      event.returnValue = '';
+      
+      
+    };
+  
+    window.addEventListener('beforeunload', handleBeforeUnload);
+    return () => {
+      window.removeEventListener('beforeunload', handleBeforeUnload);
+    };
+  }, []);
+  
+  
   
   return (
     
