a498ef860baf90c45358aa49cd4f1f89d8db772d
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-03-31 01:29:02 +0200

    master

---
+++ src/components/WeatherWidget/api.tsx
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'd05993ee04msh118ebfd9851a7dap14830ejsnb0cdd73d1ac0',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "3606097b15beb023b51e870f7eb94a02";
