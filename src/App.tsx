1ed7a866d582097ffa280108a56e3c64f9d2299a
Vineyardcode <108179228+Vineyardcode@users.noreply.github.com>
2023-04-16 00:05:42 +0200

    master

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 import  { Home }  from './pages/Home';
 import { Login } from "./pages/Login";
@@ -6,47 +6,17 @@ import { Navbar } from "./components/navbar";
 import './styles/App.css'
 import { Learn } from './pages/Learn';
 import Test from './pages/Test';
-
+import DivMaker from './components/DivMaker';
 
 
 const App = () => {
-
-  // const setBackground = () => {
-  //   let date = new Date();
-  //   let month = date.getMonth() + 1;
-  //   let day = date.getDate();
-  //   let season = "";
-  
-  //   if ((month == 2 && day >= 5) || month == 3 || month == 4 || (month == 5 && day <= 6)) {
-  //     season = "spring";
-  //   } else if ((month == 5 && day >= 7) || month == 6 || month == 7 || (month == 8 && day <= 8)) {
-  //     season = "summer";
-  //   } else if ((month == 8 && day >= 9) || month == 9 || month == 10 || (month == 11 && day <= 7)) {
-  //     season = "fall";
-  //   } else {
-  //     season = "winter";
-  //   }
-  
-  //   let imageNumber = Math.floor(Math.random() * 5) + 1;
-  //   let backgroundImage = `${season}${imageNumber}.jpg`;
-  //   let element = document.querySelector("body");
-  //   let selector = document.querySelector("selector")
-  //   element.style.backgroundImage = `url('src/assets/media/img/spring/${backgroundImage}')`;
-  //   selector.style.backgroundImage = `url('src/assets/media/img/spring/${backgroundImage}')`;
-  //   console.log(backgroundImage);
-    
-  // }
-
- 
-  // useEffect(() => {
-  //   setBackground()
-  // }, [])
-  
 
 
   return (
     
     <div className="App">
+      
+
       
       <BrowserRouter>
         <Navbar />
@@ -59,7 +29,13 @@ const App = () => {
         </Routes>
       
       </BrowserRouter>
-      
+
+      <div className='background-flag'>
+        <div className="flag-grid">
+          <DivMaker count={23} />
+        </div>
+      </div>
+
       </div>
     
    
